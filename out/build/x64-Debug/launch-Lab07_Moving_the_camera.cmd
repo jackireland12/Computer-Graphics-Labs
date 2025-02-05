03374c2a771b57d253c4f49c9bfa53a6098f1c20
jackireland12 <23685136@stu.mmu.ac.uk>
2025-02-05 15:25:33 +0000

    git ingore change

---
+++ out/build/x64-Debug/launch-Lab07_Moving_the_camera.cmd
@@ -0,0 +1,9 @@
+cd D:/git/Computer-Graphics-Labs/Lab07_Moving_the_camera/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"D:\git\Computer-Graphics-Labs\out\build\x64-Debug\Lab07_Moving_the_camera.exe"  
+pause
